c2733dae00c6b59142d83457c339453bc047f0f8
ALEXANDRE VITAL DE OLIVEIRA ARCANJO <alexandre.voarcanjo@senacsp.edu.br>
2026-08-19 10:44:11 -0300

    Exemplo de print

---
+++ Exemplo1.java
@@ -0,0 +1,10 @@
+public class Exemplo1 {
+    public static void main(String [] args){
+        System.out.println("Esse algoritmo listará meus 5 livros favoritos");
+        System.out.print("Livro 1: Homem de Giz \n");
+        System.out.print("Livro 2: Metamorfose \n");
+        System.out.print("Livro 3: Programador Pragmático \n");
+        System.out.print("Livro 4: Mitologia Nórdica \n");
+        System.out.print("Livro 5: The A.B.C. Murders \n");
+    }
+}
